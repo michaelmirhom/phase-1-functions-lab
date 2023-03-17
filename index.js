cbdf3ff1e5361e7b9a9b2e68f03bb6ff69713b89
michaelmirhom <micharby73@gmail.com>
2023-03-16 21:06:00 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    const distanceInBlocks = Math.abs(pickupLocation-hqLocation);
+    return distanceInBlocks;
+  }
+  function distanceFromHqInFeet(pickupLocation) {
+    const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+    const distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+  }
+  function  distanceTravelledInFeet(start, destination){
+    const numberTravelled =  Math.abs((destination-start)*264);
+    return numberTravelled;
+  }
+  function calculatesFarePrice(start,destination){
+    const numberTravelled = distanceTravelledInFeet(start, destination);
+    if (numberTravelled<400){
+        const charges = 0;
+        return charges;
+  }
+  else if (numberTravelled>=400 && numberTravelled<=2000){
+    const charges =  Math.abs(.02*(400-numberTravelled));    
+    return charges;
+  }
+  else if (numberTravelled>2000 && numberTravelled<=2500){
+    const charges = 25;
+    return charges;   
+  }
+  else{
+   return 'cannot travel that far' ;
+  }
+}
